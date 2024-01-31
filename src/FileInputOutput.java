4c731f680d82ad8255859466c7978eba6ae27db1
maebusko <buskmm22@wfu.edu>
2024-01-31 16:33:27 -0500

    In class exercise part 2

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,42 +1,71 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
+
 public class FileInputOutput {
 
     public static void main(String[] args) throws IOException {
 //       1) Open the given file inside src called moviesDataSet.csv
-        FileInputStream movies = null;
+//        FileInputStream movies = null;
+//        try {
+//            movies = new FileInputStream("src/moviesDataset.csv");
+//        }
+//        catch(FileNotFoundException x){
+//            System.out.println("File not found -ending program");
+//            System.exit(1);
+//        }
+//        Scanner scnr = new Scanner(movies);
+//        while(scnr.hasNextLine()){
+//            String data = scnr.nextLine();
+//            System.out.println(data);
+//        }
+//        movies.close();
+//
+//        FileInputStream movies2 = null;
+//        try {
+//            movies2 = new FileInputStream("src/moviesDataset.csv");
+//        }
+//        catch(FileNotFoundException x){
+//            System.out.println("File not found -ending program");
+//            System.exit(1);
+//        }
+//
+//        Scanner scnr2 = new Scanner(movies2);
+//        while(scnr2.hasNextLine()){
+//            String[] line = scnr2.nextLine().split(",");
+//            System.out.println(line[2]);
+//        }
+//        movies2.close();
+
+        FileInputStream movies3 = null;
         try {
-            movies = new FileInputStream("src/moviesDataset.csv");
+            movies3 = new FileInputStream("src/moviesDataset.csv");
         }
         catch(FileNotFoundException x){
             System.out.println("File not found -ending program");
             System.exit(1);
         }
-        Scanner scnr = new Scanner(movies);
-        while(scnr.hasNextLine()){
-            String data = scnr.nextLine();
-            System.out.println(data);
-        }
-        movies.close();
 
-        FileInputStream movies2 = null;
-        try {
-            movies2 = new FileInputStream("src/moviesDataset.csv");
+        Scanner scnr3 = new Scanner(movies3);
+        FileOutputStream outputYear  = new FileOutputStream("moviesDataset.csv");
+        PrintWriter fileWriter = new PrintWriter(outputYear);
+        if (scnr3.hasNextLine()){
+            scnr3.nextLine();
         }
-        catch(FileNotFoundException x){
-            System.out.println("File not found -ending program");
-            System.exit(1);
-        }
+        while(scnr3.hasNextLine()){
+            String[] line = scnr3.nextLine().split(",");
 
-        Scanner scnr2 = new Scanner(movies2);
-        while(scnr2.hasNextLine()){
-            String[] line = scnr2.nextLine().split(",");
-            System.out.println(line[2]);
+            try {
+                Integer.parseInt(line[4]);
+                fileWriter.println(line[4]);
+            }
+            catch (NumberFormatException x) {
+                fileWriter.println("N/A");
+            }
         }
+        fileWriter.close();
+        movies3.close();
 
-        movies2.close();
+
 
 //       5) Add exception handling (try & catch) when needed
 //       6) Close the files after finishing the processing
